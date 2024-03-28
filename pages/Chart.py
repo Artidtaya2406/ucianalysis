35693f954a38ca5c1d9799c0edb947e13d5f46f5
Artidtaya2406 <artidtaya2406@gmail.com>
2024-03-28 15:58:54 +0700

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
+import requests
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/09307672-f116-47b0-95b6-5b0d518c907d/17pfXgaSVf.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello,key="hello")
+
 
 #st.title("💡Website Developing using Python💡")
 #st.header("💡 Website Developing using Python 💡")
