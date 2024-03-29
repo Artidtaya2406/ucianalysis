37e1348eaf25eae1c18517ba6e5b218606247823
Artidtaya2406 <artidtaya2406@gmail.com>
2024-03-29 13:24:32 +0700

    Update home.py

--- home.py
+++ home.py
@@ -11,10 +11,10 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://lottie.host/f14ccc0b-1af9-43c6-bfed-7572d3681366/eKtdfMj042.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
-
+ st.subheader("การประยุกต์ใช้งาน Machine learning บนเว็บ By นางสาวอาทิตยา พันธ์นิล สาขาวิชาวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยีมหาวิทยาลัยราชภัฏนครปฐม")
 st.page_link("home.py", label="หน้าแรก", icon="🏠")
 st.page_link("pages/1🐠Statistic.py", label="การนำเสนอข้อมูลด้วยสถิติ", icon="1️⃣")
 st.page_link("pages/2🐸Chart.py", label="การนำเสนอข้อมูลด้วยการจินตทัศน์ข้อมูล Naive Baye", icon="2️⃣", disabled=False)
-st.page_link("pages/3🐷KNNClassification.py", label="การนำเสนอข้อมูลด้วย KNN", icon="1️⃣", disabled=False)
-st.page_link("pages/4🐱DecisionTreeClassify.py", label="การนำเสนอข้อมูลด้วย DecisionTree", icon="1️⃣", disabled=False)
+st.page_link("pages/3🐷KNNClassification.py", label="การนำเสนอข้อมูลด้วย KNN", icon="3️⃣", disabled=False)
+st.page_link("pages/4🐱DecisionTreeClassify.py", label="การนำเสนอข้อมูลด้วย DecisionTree", icon="4️⃣", disabled=False)
 st.page_link("https://www.kaggle.com/datasets/ananthr1/weather-prediction", label="ชุดข้อมูลสภาพอากาศ", icon="🌎")
