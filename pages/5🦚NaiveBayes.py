52db8f3115fd22a124d5fe91c7b83a168f020a8f
Artidtaya2406 <artidtaya2406@gmail.com>
2024-03-29 14:41:55 +0700

    Update 5🦚NaiveBayes.py

--- pages/5🦚NaiveBayes.py
+++ pages/5🦚NaiveBayes.py
@@ -25,7 +25,7 @@ X = df.drop('variety',axis=1)
 y = df['variety']
 
 html_1 = """
-<div style="background-color:#76D7C4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#F5B7B1;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h4>การนำเสนอข้อมูลด้วยเทคนิค NaiveBayes</h4></center>
 </div>
 """
