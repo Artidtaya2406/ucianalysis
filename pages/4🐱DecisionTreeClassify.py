31f91bb831895f3cb96460a357f080e91f2b6ac4
Artidtaya2406 <artidtaya2406@gmail.com>
2024-03-29 12:03:34 +0700

    Update 4🐱DecisionTreeClassify.py

--- pages/4🐱DecisionTreeClassify.py
+++ pages/4🐱DecisionTreeClassify.py
@@ -9,9 +9,9 @@ cl1,cl2,cl3=st.columns(3)
 with cl1:
     st.image('./pic/Setosa.jpg')
 with cl2:
-     st.image('./pic/Virginica.jpg')
-with cl3:  
      st.image('./pic/Versicolor.jpg')
+with cl3:  
+     st.image('./pic/Virginica.jpg')
 st.header("Decision Tree for classification")
 df = pd.read_csv("./data/iris.csv")
 st.write(df.head(10))
